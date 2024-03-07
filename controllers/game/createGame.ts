7112c6c8e70b9296a9d70e4b5934dfa160605e23
SebastianZasadni <emblelele@gmail.com>
2024-03-07 08:57:01 +0100

    feat/validate createGame, loginGuest

--- controllers/game/createGame.ts
+++ controllers/game/createGame.ts
@@ -1,10 +1,29 @@
 import { NextFunction, Response, Request } from "express";
+import Joi from "joi";
 import Game from "../../models/game";
+
+const gameSchema = Joi.object({
+  playTime: Joi.number().min(5).max(20).required(),
+  name: Joi.string().min(3).max(30).required().alphanum(),
+  numberOfPlayers: Joi.number().min(2).max(4).required(),
+  id: Joi.string().required(),
+});
 
 const createGame = async (req: any, res: Response, next: NextFunction) => {
   try {
     const { playTime, numberOfPlayers, name } = req.body;
     const { id } = req.user;
+    const validate = gameSchema.validate({
+      playTime,
+      numberOfPlayers,
+      name,
+      id,
+    });
+    if (!!validate.error) {
+      return res.status(422).json({
+        error: validate.error.message,
+      });
+    }
     const userCreatedGame = await Game.find({ ownerId: id });
     if (!!userCreatedGame.length) {
       return res.status(409).json({
